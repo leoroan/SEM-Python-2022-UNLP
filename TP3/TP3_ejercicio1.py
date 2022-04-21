049d816c0cad2825f499e685413f095324217e18
Leandro R <leoroan@gmail.com>
2022-04-21 17:42:29 -0300

    ejercicio1 - tp 3

--- TP3/TP3_ejercicio1.py
+++ TP3/TP3_ejercicio1.py
@@ -2,6 +2,7 @@
 import csv
 import os
 
+
 # Establecer nombre de la carpeta.
 path_file = "archivos_tp3"
 # Establecer el nombre del archivo y su extension.
@@ -17,8 +18,6 @@ data_net = csv.reader(archivo, delimiter=',')
 # Guarda en Header los datros cabecera y en datos la lista con lo que intregra al archivo (lector csv)
 header, datos = next(data_net), list(data_net)
 
-# print(header)
-
 """
   1. Encontrar qué tipo de shows tiene un país determinado.
     • Realizar una función que informe todos los países que existen.
@@ -31,3 +30,47 @@ header, datos = next(data_net), list(data_net)
     • Como en algunos casos hay varios países en un show debemos separarlos y quedarnos con
       valores únicos.
 """
+def obtener_paises(una_lista,pos):
+
+  paises = list(map(lambda fila: fila[pos].split(","), una_lista))
+  aux = set()
+  for elem in paises:
+    if elem !=['']:
+      for x in elem:
+        if x !='':
+          aux.add(x)
+  return aux
+
+# dado una lista, y la posicion de la columna de paises, obtenemos los paises existentes en el documento, en un listado.
+
+print('Paises existentes, disponibles:')
+
+lista = []
+for elem in obtener_paises(datos,5):
+  lista.append(elem)
+lista.sort()
+
+print(lista)
+
+# Realizar una función que dado un país informe si es parte de la línea del show pasado como argumento
+
+aux = []
+print('Lista de "shows": ', )
+for elem in range(len(datos)):
+  if datos[elem][1] not in aux:
+    aux.append(datos[elem][1])
+print(aux)    
+
+show_pasado = "Movie"
+es_parte = map(lambda x : x if x in datos[0:][5] else "no",  obtener_paises(datos,5))
+
+
+print(list(es_parte))
+
+
+
+
+
+
+
+
