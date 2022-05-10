79b14d204784098f75fb2ec3137c42824805388b
Leandro R <leoroan@gmail.com>
2022-05-10 20:20:59 -0300

    agregado la clase 9 - desafio 3

---
+++ DESAFIOS TEORIA/clase9_desafio3.py
@@ -0,0 +1,21 @@
+import os
+import pandas as pd
+
+
+url = "https://es.wikipedia.org/wiki/Copa_Mundial_de_F%C3%BAtbol"
+df = pd.read_html(url)
+
+#El df[4] es el que quiero para trabajar...
+goleadores = df[4]
+
+# Elimino la primera columna que repetía el indice
+goleadores = goleadores.drop(goleadores.columns[0],axis=1)
+
+# Quiero saber qué goleadores hicieron más de 5 goles
+masde = goleadores [goleadores["Goles"] > 11 ]
+
+# corroboro los datos
+print(masde)
+
+# los salvo
+masde.to_csv(os.path.join(os.getcwd(),'resultadoGoleadores.csv'))
